caa905e6989d9f30d52a1fee9044b1ee977215ea
deepanshutripathi109-ui <deepanshutripathi109@gmail.com>
2026-04-24 09:19:21 +0530

    24

---
+++ react.js/my-app12/src/App.jsx
@@ -0,0 +1,20 @@
+function App(){
+  return(
+    <div className="App">
+      <h1>Hello World</h1>
+      <p>This is my React app.</p>
+      <Cse12/>
+    </div>
+  );
+}
+function Cse12(){
+  return(
+    <div className="Cse12">
+      <h1>Welcome to Cse12</h1>
+      <p>This is the a second component.</p>
+    </div>
+  );
+}
+
+
+export default App;
